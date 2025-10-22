45739227d08e88b4cb00f147a3436e0f9be76d18
Juan C. Correa <j.correa.n@gmail.com>
2025-10-21 21:18:26 -0600

    Update InferentialModels.R

--- InferentialModels.R
+++ InferentialModels.R
@@ -4,7 +4,9 @@ library(ergm)
 library(ergm.multi)
 library(coda)
 SampledNetworks <- Networks(RegionalNetworks)
+RegionalNetworks
 SampledNetworks
+get.vertex.attribute(SampledNetworks, "Country")
 set.seed(2758)
 model0 <- ergm(SampledNetworks ~ edges + b1factor('Program', levels = c("Doctorado", "Maestría")))
 summary(model0)
