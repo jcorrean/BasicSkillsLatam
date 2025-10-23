1b539bee5d4602804d0cf4096a4ff2ffb4be670e
Juan C. Correa <j.correa.n@gmail.com>
2025-10-23 13:58:05 -0600

    Update Comparison.R

--- Comparison.R
+++ Comparison.R
@@ -130,6 +130,76 @@ ggplot(Skills, aes(Level, Node, fill= Eigenvector)) +
   labs(fill="Centrality")
 
 
+load("Results/Argentina.RData")
+load("Results/Brazil.RData")
+load("Results/Chile.RData")
+load("Results/Colombia.RData")
+load("Results/CostaRica.RData")
+load("Results/Ecuador.RData")
+load("Results/Mexico.RData")
+load("Results/Uruguay.RData")
+load("Results/Venezuela.RData")
+
+rm(list=setdiff(ls(), c("ProgramsARG1", "ProgramsARG2", "ProgramsARG3",
+                        "ProgramsBRA1", "ProgramsBRA2", "ProgramsBRA3",
+                        "ProgramsCHL1", "ProgramsCHL2", "ProgramsCHL3",
+                        "ProgramsCOL1", "ProgramsCOL2", "ProgramsCOL3",
+                        "ProgramsCORI1", "ProgramsCORI2", "ProgramsCORI3",
+                        "ProgramsECU1", "ProgramsECU2", "ProgramsECU3",
+                        "ProgramsMEX1", "ProgramsMEX2", "ProgramsMEX3",
+                        "ProgramsURU1", "ProgramsURU2", "ProgramsURU3",
+                        "ProgramsVEN1", "ProgramsVEN2", "ProgramsVEN3")))
+
+AllPrograms <- do.call(rbind, list(ProgramsARG1,
+                                   ProgramsARG2,
+                                   ProgramsARG3,
+                                   ProgramsBRA1,
+                                   ProgramsBRA2,
+                                   ProgramsBRA3,
+                                   ProgramsCHL1,
+                                   ProgramsCHL2,
+                                   ProgramsCHL3,
+                                   ProgramsCOL1,
+                                   ProgramsCOL2,
+                                   ProgramsCOL3,
+                                   ProgramsCORI1,
+                                   ProgramsCORI2,
+                                   ProgramsCORI3,
+                                   ProgramsECU1,
+                                   ProgramsECU2,
+                                   ProgramsECU3,
+                                   ProgramsMEX1,
+                                   ProgramsMEX2,
+                                   ProgramsMEX3,
+                                   ProgramsURU1,
+                                   ProgramsURU2,
+                                   ProgramsURU3,
+                                   ProgramsVEN1,
+                                   ProgramsVEN2,
+                                   ProgramsVEN3))
+
+png(filename = "F1.png", width = 40, height = 18, units = "in", res = 300)
+ggplot(AllPrograms, aes(x=Level, y=Eigenvector, fill = Partition)) +
+  geom_violin() +
+  scale_x_discrete(limits = c("Specialization", "Master", "PhD")) +
+  facet_wrap(~Country) +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        legend.title=element_text(size=50), 
+        legend.text=element_text(size=50),
+        axis.title.x = element_text(size = 30, colour = "black"),
+        axis.title.y = element_text(size = 30, colour = "black"),
+        axis.text.x = element_text(size = 30, colour = "black"),
+        axis.text.y = element_text(size = 30, colour = "black"),
+        strip.text = element_text(face="bold", size=rel(3.5), colour = "black"),
+        strip.background = element_rect(fill="grey", colour="grey",
+                                        size=30)) +
+  xlab("") + ylab("Normalized centrality degree") +
+  labs(fill="")+
+  scale_fill_manual(values = c("Skill" = "#09419e", "Program" = "red2"))
+dev.off()
+
+
 library(ergm.multi)
 SampledNetworks <- Networks(RegionalNetworks)
 # debugging
