36d425acd59cca6636b91d81deb4ea6ac77422e4
Juan C. Correa <j.correa.n@gmail.com>
2025-12-14 13:00:56 -0600

    Update NetworksByCountry.R

--- NetworksByCountry.R
+++ NetworksByCountry.R
@@ -36,3 +36,60 @@ result <- AllCountries %>%
   )
 result
 
+
+
+load("Results/Argentina.RData")
+load("Results/Brazil.RData")
+load("Results/Chile.RData")
+load("Results/Colombia.RData")
+load("Results/CostaRica.RData")
+load("Results/Ecuador.RData")
+load("Results/Mexico.RData")
+load("Results/Uruguay.RData")
+load("Results/Venezuela.RData")
+
+rm(list=setdiff(ls(), c("ProgramsARG1", "ProgramsARG2", "ProgramsARG3",
+                        "ProgramsBRA1", "ProgramsBRA2", "ProgramsBRA3",
+                        "ProgramsCHL1", "ProgramsCHL2", "ProgramsCHL3",
+                        "ProgramsCOL1", "ProgramsCOL2", "ProgramsCOL3",
+                        "ProgramsCORI1", "ProgramsCORI2", "ProgramsCORI3",
+                        "ProgramsECU1", "ProgramsECU2", "ProgramsECU3",
+                        "ProgramsMEX1", "ProgramsMEX2", "ProgramsMEX3",
+                        "ProgramsURU1", "ProgramsURU2", "ProgramsURU3",
+                        "ProgramsVEN1", "ProgramsVEN2", "ProgramsVEN3")))
+
+AllPrograms <- do.call(rbind, list(ProgramsARG1,
+                                   ProgramsARG2,
+                                   ProgramsARG3,
+                                   ProgramsBRA1,
+                                   ProgramsBRA2,
+                                   ProgramsBRA3,
+                                   ProgramsCHL1,
+                                   ProgramsCHL2,
+                                   ProgramsCHL3,
+                                   ProgramsCOL1,
+                                   ProgramsCOL2,
+                                   ProgramsCOL3,
+                                   ProgramsCORI1,
+                                   ProgramsCORI2,
+                                   ProgramsCORI3,
+                                   ProgramsECU1,
+                                   ProgramsECU2,
+                                   ProgramsECU3,
+                                   ProgramsMEX1,
+                                   ProgramsMEX2,
+                                   ProgramsMEX3,
+                                   ProgramsURU1,
+                                   ProgramsURU2,
+                                   ProgramsURU3,
+                                   ProgramsVEN1,
+                                   ProgramsVEN2,
+                                   ProgramsVEN3))
+
+library(psych)
+
+Skills <- AllPrograms %>% filter(., Partition == "Skill")
+
+
+pave <- describeBy(Skills$Degree, group = Country, mat = TRUE, digits = 2)
+pave
