c8c7ae9892d4d68238d0cc6ce62b4dc46df4d750
Juan C. Correa <j.correa.n@gmail.com>
2026-06-20 20:14:50 -0600

    Update InferentialModels.R

--- InferentialModels.R
+++ InferentialModels.R
@@ -48,3 +48,17 @@ model4 <- ergm(LatamNet ~ edges +
                                       MCMC.burnin = 10000, 
                                       MCMLE.maxit = 10))
 summary(model4) # AIC: 36919
+
+
+model5 <- ergm(LatamNet ~ 
+                 edges +
+                 b2factor("vertex.names",
+                          levels = c("science","speaking",
+                                     "reading_comprehension",
+                                     "active_listening","learning_strategy")) +
+                 b1cov("Brochure.Length") +
+                 b1factor("Program", levels = c("Master","PhD")) +
+                 b1cov("OECD") +
+                 gwb2degree(decay = 0.5, fixed = TRUE) +
+                 gwb1degree(decay = 0.5, fixed = TRUE)
+)
