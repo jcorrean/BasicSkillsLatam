2a5d26a684f30806219f975c6d3e165481d3255a
Juan C. Correa <j.correa.n@gmail.com>
2026-07-03 10:19:16 -0600

    Update InferentialModels.R

--- InferentialModels.R
+++ InferentialModels.R
@@ -2,13 +2,43 @@ library(network)
 load("NetworkData/LatamNetwork.RData")
 library(ergm)
 library(coda)
-#SampledNetworks <- Networks(RegionalNetworks)
+library(parallel)
+n_cores <- detectCores() - 1 
+backend_control <- control.ergm(
+  MCMC.samplesize = 50000, # Reducido de 100k a 50k: óptimo para convergencia sin quemar RAM
+  MCMC.burnin = 10000, 
+  MCMLE.maxit = 15,        # Un poco más de margen para ayudar a la convergencia
+  parallel = n_cores, 
+  parallel.type = "PSOCK"
+)
 set.seed(2758)
-model1 <- ergm(LatamNet ~ edges + b1cov("Brochure.Length"), 
-               control = control.ergm(MCMC.samplesize = 100000, 
-                                      MCMC.burnin = 10000, 
-                                      MCMLE.maxit = 10))
+model1 <- ergm(LatamNet ~ edges,
+               control = backend_control)
 summary(model1)
+
+model2 <- ergm(LatamNet ~ edges + 
+                 b1cov("Brochure.Length"), 
+               control = backend_control)
+summary(model2)
+
+model3 <- ergm(LatamNet ~ edges + 
+                 b1cov("Brochure.Length") +
+                 gwb1degree(decay = 0.5, fixed = TRUE),
+               control = backend_control)
+mcmc.diagnostics(model3)
+summary(model3)
+
+
+model4 <- ergm(LatamNet ~ edges + 
+                 b1cov("Brochure.Length") +
+                 gwb1degree(decay = 0.5, fixed = TRUE) +
+                 b2factor("vertex.names", levels=c("science",
+                                                   "speaking", 
+                                                   "reading_comprehension", 
+                                                   "active_listening",
+                                                   "learning_strategy")),
+               control = backend_control)
+summary(model4)
 
 model2 <- ergm(LatamNet ~ edges +
                  b2factor("vertex.names", levels=c("science",
@@ -49,15 +79,7 @@ model4 <- ergm(LatamNet ~ edges +
                                       MCMLE.maxit = 10))
 summary(model4) # AIC: 36919
 
-library(parallel)
-n_cores <- detectCores() - 1 
-backend_control <- control.ergm(
-  MCMC.samplesize = 50000, # Reducido de 100k a 50k: óptimo para convergencia sin quemar RAM
-  MCMC.burnin = 10000, 
-  MCMLE.maxit = 15,        # Un poco más de margen para ayudar a la convergencia
-  parallel = n_cores, 
-  parallel.type = "PSOCK"
-)
+
 
 model5_fixed <- ergm(LatamNet ~ edges + 
                        b2factor("vertex.names", levels = c("science", "speaking", 
