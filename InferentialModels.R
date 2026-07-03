4a5e963952420e8db12d25db1e05991d4ca8f2ae
Juan C. Correa <j.correa.n@gmail.com>
2026-07-03 10:33:46 -0600

    Update InferentialModels.R

--- InferentialModels.R
+++ InferentialModels.R
@@ -40,57 +40,30 @@ model4 <- ergm(LatamNet ~ edges +
                control = backend_control)
 summary(model4)
 
-model2 <- ergm(LatamNet ~ edges +
+model5 <- ergm(LatamNet ~ edges + 
+                 b1cov("Brochure.Length") +
+                 gwb1degree(decay = 0.5, fixed = TRUE) +
                  b2factor("vertex.names", levels=c("science",
                                                    "speaking", 
                                                    "reading_comprehension", 
                                                    "active_listening",
                                                    "learning_strategy")) +
-                 b1cov("Brochure.Length"),               
-               control = control.ergm(MCMC.samplesize = 100000,
-                                      MCMC.burnin = 10000, 
-                                      MCMLE.maxit = 10))
-summary(model2)
-
-model3 <- ergm(LatamNet ~ edges + 
-                 b2factor("vertex.names", levels=c("science",
-                                                   "speaking",
-                                                   "active_listening",
-                                                   "reading_comprehension",
-                                                   "learning_strategy")) +
-                 b1cov("Brochure.Length") +
                  b1factor("Program", levels=c("Master", "PhD")),
-               control = control.ergm(MCMC.samplesize = 100000,
-                                      MCMC.burnin = 10000, 
-                                      MCMLE.maxit = 10))
-summary(model3) # AIC: 36919
+               control = backend_control)
+summary(model5)
 
-model4 <- ergm(LatamNet ~ edges + 
+model6 <- ergm(LatamNet ~ edges + 
+                 b1cov("Brochure.Length") +
+                 gwb1degree(decay = 0.5, fixed = TRUE) +
                  b2factor("vertex.names", levels=c("science",
-                                                   "speaking",
-                                                   "reading_comprehension",
+                                                   "speaking", 
+                                                   "reading_comprehension", 
                                                    "active_listening",
                                                    "learning_strategy")) +
-                 b1cov("Brochure.Length") +
-                 b1factor("Program", levels=c("Master", "PhD"))+
+                 b1factor("Program", levels=c("Master", "PhD")) +
                  b1cov("OECD"),
-               control = control.ergm(MCMC.samplesize = 100000,
-                                      MCMC.burnin = 10000, 
-                                      MCMLE.maxit = 10))
-summary(model4) # AIC: 36919
-
-
-
-model5_fixed <- ergm(LatamNet ~ edges + 
-                       b2factor("vertex.names", levels = c("science", "speaking", 
-                                                           "reading_comprehension", 
-                                                           "active_listening", 
-                                                           "learning_strategy")) +
-                       b1cov("Brochure.Length") +
-                       b1factor("Program", levels = c("Master", "PhD")) +
-                       b1cov("OECD") +
-                       gwb1degree(decay = 0.5, fixed = TRUE), # Aplicado al MODO 1 (Programas)
-                     control = backend_control)
-summary(model5_fixed)
+               control = backend_control)
+summary(model6)  
+  
 
 save.image("~/Documents/GitHub/BasicSkillsLatam/Inferential_Results.RData")
